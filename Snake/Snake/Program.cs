620fcb4ff31c3bef0a0ce8ea72d5b43da61d9d08
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-12-15 19:18:32 +0200

    added key handling

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Snake
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.SetBufferSize( 80, 25 );
+
+			// Отрисовка рамочки
+			HorizontalLine upLine = new HorizontalLine( 0, 78, 0, '+' );
+			HorizontalLine downLine = new HorizontalLine( 0, 78, 24, '+' );
+			VerticalLine leftLine = new VerticalLine( 0, 24, 0, '+' );
+			VerticalLine rightLine = new VerticalLine( 0, 24, 78, '+' );
+			upLine.Draw();
+			downLine.Draw();
+			leftLine.Draw();
+			rightLine.Draw();
+
+
+			// Отрисовка точек			
+			Point p = new Point( 4, 5, '*' );
+			Snake snake = new Snake( p, 4, Direction.RIGHT );
+			snake.Draw();
+
+			while(true)
+			{
+				if(Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey( key.Key );
+				}
+				Thread.Sleep( 100 );
+				snake.Move();
+			}
+		}
+	}
+}
